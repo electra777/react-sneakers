c6eb88e084022765b48eff47e6c6dc5f19698140
electra777 <electra0789@gmail.com>
2023-05-28 17:23:08 +0300

    added CartDrawer

---
+++ src/components/CartDrawer/CartDrawer.js
@@ -0,0 +1,55 @@
+function CartDrawer() {
+	return (
+		<div className="drawer">
+			<div className="cartTop">
+				<h2 className="cartTitle">Корзина</h2>
+				<button className="remove"></button>
+			</div>
+			<div className="items">
+				<div className="cartItem">
+					<img src="/img/sneakers/1.jpg" alt="sneakers" width={70} height={70} />
+					<div className="cartDiscriptionWrapper">
+						<p className="cartDiscription">Мужские Кроссовки Nike Air Max 270</p>
+						<div className="priceSum">
+							12 999<span> руб.</span>
+						</div>
+					</div>
+					<button className="remove"></button>
+				</div>
+				<div className="cartItem">
+					<img src="/img/sneakers/1.jpg" alt="sneakers" width={70} height={70} />
+					<div className="cartDiscriptionWrapper">
+						<p className="cartDiscription">Мужские Кроссовки Nike Air Max 270</p>
+						<div className="priceSum">
+							12 999<span> руб.</span>
+						</div>
+					</div>
+					<button className="remove"></button>
+				</div>
+			</div>
+
+			<div className="cartTotalBlock">
+				<div className="infoSum">
+					<p>Итого:</p>
+					<div className="dashed"></div>
+					<div className="totalSum">
+						21 498<span> руб.</span>
+					</div>
+				</div>
+				<div className="infoSum">
+					<p>Налог 5%: </p>
+					<div className="dashed"></div>
+					<div className="totalSum">
+						1 074<span> руб.</span>
+					</div>
+				</div>
+				<button className="greenBtn">
+					Оформить заказ
+					<img src="/img/arrow.svg" alt="arrow"></img>
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default CartDrawer;

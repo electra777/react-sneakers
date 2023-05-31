7ddd689de9644193a8be70b51483353ff99d7927
electra777 <electra0789@gmail.com>
2023-05-31 21:52:33 +0300

    added Card css module

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,27 @@
+import cn from 'classnames';
+import styles from './Card.module.scss';
+
 function Card() {
 	return (
-		<div className="card">
-			<button className="favorite liked"></button>
+		<div className={styles.card}>
+			<button className={cn(styles.favorite, styles.liked)}></button>
 
 			<img
 				width={133}
 				height={112}
 				src="/img/sneakers/1.jpg"
 				alt="sneakers"
-				className="sneakersImg"
+				className={styles.sneakersImg}
 			/>
-			<p className="description">Мужские Кроссовки Nike Blazer Mid Suede</p>
-			<div className="bottomWrapper">
-				<div className="priceWrapper">
-					<div className="price">Цена:</div>
-					<div className="priceSum">
+			<p className={styles.description}>Мужские Кроссовки Nike Blazer Mid Suede</p>
+			<div className={styles.bottomWrapper}>
+				<div className={styles.priceWrapper}>
+					<div className={styles.price}>Цена:</div>
+					<div className={styles.priceSum}>
 						12 999<span> руб.</span>
 					</div>
 				</div>
-				<button className="btnCart inCart"></button>
+				<button className={cn(styles.btnCart, styles.inCart)}></button>
 			</div>
 		</div>
 	);

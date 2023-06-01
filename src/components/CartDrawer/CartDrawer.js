0ea5e831fa11b79089c72540b23515346c1d5d22
electra777 <electra0789@gmail.com>
2023-06-01 22:08:51 +0300

    added CartDrawer css module

--- src/components/CartDrawer/CartDrawer.js
+++ src/components/CartDrawer/CartDrawer.js
@@ -1,51 +1,53 @@
+import styles from './CartDrawer.module.scss';
+
 function CartDrawer() {
 	const a = 0;
 	return (
-		<div style={{ display: 'none' }} className="overlay">
-			<div className="drawer">
-				<div className="cartTop">
-					<h2 className="cartTitle">Корзина</h2>
-					<button className="remove"></button>
+		<div style={{ display: 'block' }} className={styles.overlay}>
+			<div className={styles.drawer}>
+				<div className={styles.cartTop}>
+					<h2 className={styles.cartTitle}>Корзина</h2>
+					<button className={styles.remove}></button>
 				</div>
-				<div className="items">
-					<div className={a}>
+				<div className={styles.items}>
+					<div className={styles.cartItem}>
 						<img src="/img/sneakers/1.jpg" alt="sneakers" width={70} height={70} />
-						<div className="cartDiscriptionWrapper">
-							<p className="cartDiscription">Мужские Кроссовки Nike Air Max 270</p>
-							<div className="priceSum">
+						<div className={styles.cartDiscriptionWrapper}>
+							<p className={styles.cartDiscription}>Мужские Кроссовки Nike Air Max 270</p>
+							<div className={styles.priceSum}>
 								12 999<span> руб.</span>
 							</div>
 						</div>
-						<button className="remove"></button>
+						<button className={styles.remove}></button>
 					</div>
-					<div className="cartItem">
+					<div className={styles.cartItem}>
 						<img src="/img/sneakers/1.jpg" alt="sneakers" width={70} height={70} />
-						<div className="cartDiscriptionWrapper">
-							<p className="cartDiscription">Мужские Кроссовки Nike Air Max 270</p>
-							<div className="priceSum">
+						<div className={styles.cartDiscriptionWrapper}>
+							<p className={styles.cartDiscription}>Мужские Кроссовки Nike Air Max 270</p>
+							<div className={styles.priceSum}>
 								12 999<span> руб.</span>
 							</div>
 						</div>
-						<button className="remove"></button>
+						<button className={styles.remove}></button>
 					</div>
 				</div>
 
-				<div className="cartTotalBlock">
-					<div className="infoSum">
+				<div className={styles.cartTotalBlock}>
+					<div className={styles.infoSum}>
 						<p>Итого:</p>
-						<div className="dashed"></div>
-						<div className="totalSum">
+						<div className={styles.dashed}></div>
+						<div className={styles.totalSum}>
 							21 498<span> руб.</span>
 						</div>
 					</div>
-					<div className="infoSum">
+					<div className={styles.infoSum}>
 						<p>Налог 5%: </p>
-						<div className="dashed"></div>
-						<div className="totalSum">
+						<div className={styles.dashed}></div>
+						<div className={styles.totalSum}>
 							1 074<span> руб.</span>
 						</div>
 					</div>
-					<button className="greenBtn">
+					<button className={styles.greenBtn}>
 						Оформить заказ
 						<img src="/img/arrow.svg" alt="arrow"></img>
 					</button>

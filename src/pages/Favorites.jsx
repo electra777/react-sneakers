3c16bb7608ced8b8f4c036b80c2a823adf0f2d34
electra777 <electra0789@gmail.com>
2024-07-19 05:37:13 +0300

    added fixes

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -5,28 +5,41 @@ import Card from '../components/Card/Card';
 function Favorites({ favoriteItems = [], handleClickPlus, handleClickFavorite }) {
 	return (
 		<div>
-			<div className="top">
-				<Link to="/">
-					<BtnBack />
-				</Link>
-				<h1 className="title">Мои закладки</h1>
-			</div>
+			{favoriteItems.length !== 0 ? (
+				<>
+					<div className="top">
+						<Link to="/">
+							<BtnBack />
+						</Link>
+						<h1 className="title">Мои закладки</h1>
+					</div>
 
-			<div className="content">
-				{favoriteItems.map((item) => {
-					return (
-						<Card
-							key={item.id}
-							id={item.id}
-							title={item.title}
-							price={item.price}
-							imageUrl={item.imageUrl}
-							onAddToCart={() => handleClickPlus(item)}
-							onAddToFavorite={() => handleClickFavorite(item)}
-						/>
-					);
-				})}
-			</div>
+					<div className="content">
+						{favoriteItems.map((item) => {
+							return (
+								<Card
+									key={item.id}
+									id={item.id}
+									title={item.title}
+									price={item.price}
+									imageUrl={item.imageUrl}
+									onAddToCart={() => handleClickPlus(item)}
+									onAddToFavorite={() => handleClickFavorite(item)}
+								/>
+							);
+						})}
+					</div>
+				</>
+			) : (
+				<div className="contentEmpty">
+					<div className="sadSmile"></div>
+					<h2 className="sadTitle">Закладок нет :(</h2>
+					<div className="sadText">Вы ничего не добавляли в закладки</div>
+					<Link to="/">
+						<BtnBack />
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 }
